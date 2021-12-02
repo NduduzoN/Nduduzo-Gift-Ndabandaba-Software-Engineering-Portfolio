549467eb9fd005dd42e7d9a6e4fa1bfed962aac0
Nduduzo Ndabandaba <giftnduduzo@gmail.com>
2021-12-02 10:39:57 +0200

    fixing image

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,8 +2,8 @@ export const projects = [
   {
     title: 'Clean Cuts',
     description: "Using PHP MVC to manage model,view,and controler ,I used a combination of bootstrap and css stylsheet to make everything beautiful, and mysqli for the database ",
-      image: '/public/images/1.jpg',
-      tags: ['PHP MVC', 'CSS', 'bootstrap', 'HTML'],
+      image: 'Nduduzo-Gift-Ndabandaba-Software-Engineering-Portfolio/public/images/1.JPG',
+      tags: ['PHP MVC', 'CSS', 'Bootstrap', 'HTML'],
     source: 'https://google.com',
     visit: ' https://github.com/NduduzoN/cleanCuts.git',
     id: 0,
@@ -13,7 +13,7 @@ export const projects = [
 export const TimeLineData = [
   { year: 2016, text: 'Realized my destiny in software development', },
   { year: 2017, text: 'Began learning sql and linux ubuntu', },
-  { year: 2019, text: 'Tought myself javascript', },
+  { year: 2019, text: 'Tought myself javascript ES6', },
   { year: 2020, text: 'Did my in-service trainning PHP MVC ', },
   { year: 2021, text: 'Graduated from Tshwane Univerity Of Technology', },
 ];
